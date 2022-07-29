4a8a1dde73df65f24f60c9063f5a18cde55f396a
Kevin-Jimenez <ksjimenez63@misena.edu.co>
2022-07-28 20:46:51 -0500

    Loading First Campus

---
+++ src/First/Main.java
@@ -0,0 +1,25 @@
+package First;
+
+public class Main {
+    public static void main(String[] args) {
+        int integer= 12;
+        boolean trueorfalse = false;
+        byte bite = 1;
+        char charA= 'A';
+        short s = 56;
+        float flo= 7.99f;
+        double dou = 4.445;
+        String str= "Hello World";
+        long li = 4545455;
+
+        System.out.println("Int -> "+integer);
+        System.out.println("Boolean -> "+trueorfalse);
+        System.out.println("Bity -> "+bite);
+        System.out.println("Char -> "+charA);
+        System.out.println("Short -> "+s);
+        System.out.println("Float -> "+flo);
+        System.out.println("Double -> "+dou);
+        System.out.println("String -> "+str);
+        System.out.println("Long -> "+li);
+    }
+}
